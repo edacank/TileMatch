3aafdcde67db4cb80cce2c13497301a2a3ca8f43
edacank <edacan1924@gmail.com>
2025-01-23 19:43:24 +0300

    sum
    Board.cs and Tile.cs have been added

---
+++ Tile-Match/Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    Tile[,] allTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new Tile[width, height];
+        SetUpTiles();
+    }
+
+    
+    void SetUpTiles(){
+        for(int i =0; i < width; i++){
+            for(int j=0; j < height; j++){
+                GameObject tile= Instantiate(tilePrefab, new Vector3(i,j,0),Quaternion.identity) as GameObject;
+                tile.name = "Tile(" + i +"," +j+ ")";
+                allTiles [i,j] =tile.GetComponent<Tile>();
+                tile.transform.parent =  transform;
+            }
+        }
+    }
+}
